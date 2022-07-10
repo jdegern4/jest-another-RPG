306dc6b60ab077c00a40e2b94cca6710f63e03f8
Jordan D <degco44@gmail.com>
2022-07-09 21:17:06 -0500

    prototype functions for player's health

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -32,3 +32,32 @@ test('get inventory from player or returns false', () => {
     player.inventory = [];
     expect(player.getInventory()).toEqual(false);
 });
+
+test("gets player's health value", () => {
+    const player = new Player('Dave');
+
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+});
+
+test('checks whether or not a player is alive', () => {
+    const player = new Player('Dave');
+    
+    expect(player.isAlive()).toBeTruthy();
+    
+    player.health = 0;
+
+    expect(player.isAlive()).toBeFalsy();
+});
+
+test("subtracts from player's health", () => {
+    const player = new Player('Dave');
+    const oldHealth = player.health;
+
+    player.reduceHealth(5);
+
+    expect(player.health).toBe(oldHealth - 5);
+
+    player.reduceHealth(99999);
+
+    expect(player.health).toBe(0);
+});
